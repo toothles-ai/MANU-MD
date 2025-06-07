641b944fcadd085716c609057d906b65dc9b2bdf
toothles-ai <tharushanamadara0000@gmail.com>
2025-06-07 19:24:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -41,6 +41,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Put_Your_Session_ID', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&qVdgRAwC#aQFt4RsooWdsSaS8T-UafEPBMJz_HnTy-0bgd9oNSmA', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000'
 };
